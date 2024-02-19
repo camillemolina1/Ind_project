32047c0e5a7abb7679e597398b08946303d5da24
camillemolina1 <hy21734@bristol.ac.uk>
2024-02-19 15:10:04 +0000

    working starter money model

--- starter_model/run.py
+++ starter_model/run.py
@@ -1,25 +1,18 @@
-from money_model import MoneyModel
+from agent import MoneyModel
 import mesa
-
-# Data visualization tools.
-import seaborn as sns
-
-# Has multi-dimensional arrays and matrices. Has a large collection of
-# mathematical functions to operate on these arrays.
 import numpy as np
-
-# Data manipulation and analysis.
 import pandas as pd
+import seaborn as sns   # data visualization tools
 
 model = MoneyModel(100, 10, 10)
 for i in range(20):
     model.step()
 
-agent_counts = np.zeros((model.grid.width, model.grid.height))
-for cell_content, (x, y) in model.grid.coord_iter():
-    agent_count = len(cell_content)
-    agent_counts[x][y] = agent_count
-# Plot using seaborn, with a size of 5x5
-g = sns.heatmap(agent_counts, cmap="viridis", annot=True, cbar=False, square=True)
-g.figure.set_size_inches(4, 4)
-g.set(title="Number of agents on each cell of the grid");
+# agent_counts = np.zeros((model.grid.width, model.grid.height))
+# for cell_content, (x, y) in model.grid.coord_iter():
+#     agent_count = len(cell_content)
+#     agent_counts[x][y] = agent_count
+# # Plot using seaborn, with a size of 5x5
+# g = sns.heatmap(agent_counts, cmap="viridis", annot=True, cbar=False, square=True)
+# g.figure.set_size_inches(4, 4)
+# g.set(title="Number of agents on each cell of the grid");

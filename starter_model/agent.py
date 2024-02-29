b7d60baf27f421716627e625dcf420e920bdf71f
camillemolina1 <hy21734@bristol.ac.uk>
2024-02-29 17:33:47 +0100

    if an agent is near an apply that is not fullt eaten it will stay close to it and if it is near one it will go towards it kind of

--- starter_model/agent.py
+++ starter_model/agent.py
@@ -4,42 +4,58 @@ import mesa
 class Food(mesa.Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.food = 20
+        self.supply = 20
 
 
 class HungryAgent(mesa.Agent):
 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.food = 0
+        self.hunger = 0
 
     def step(self):
-        if self.food < 0:
-            self.get_food()
-        if self.food > -40:
+        if self.hunger > 0:
+            self.eat()
+        if self.hunger < 40:
             self.move()
-            self.food -= 1
+            self.hunger += 1
 
-    def get_food(self):
+    def eat(self):
         # check if food nearby & take it
         neighborhood = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=True)
-        cellmates = self.model.grid.get_cell_list_contents(neighborhood)
-        for c in cellmates:
-            if isinstance(c, Food) and c.food > 0:
-                c.food -= 10
-                if c.food < -40:
-                    self.model.grid.remove_agent(c)
-                    self.model.schedule.remove(c)
-                self.food += 10
+        neighbor = self.model.grid.get_cell_list_contents(neighborhood)
+        for n in neighbor:
+            if isinstance(n, Food) and n.supply > 0:
+                n.supply -= 10
+                self.hunger -= 20
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
             self.pos, moore=False, include_center=True
         )
-        new_position = self.random.choice(possible_steps)
+        new_position = self.choose_move(possible_steps)
         neighbour = self.model.grid.get_cell_list_contents(new_position)
         while len(neighbour) != 0:
             new_position = self.random.choice(possible_steps)
             neighbour = self.model.grid.get_cell_list_contents(new_position)
-        print(new_position, neighbour, len(neighbour))
         self.model.grid.move_agent(self, new_position)
+
+    def choose_move(self, possible_steps):
+        # Find viable random step
+        best_move = self.random.choice(possible_steps)
+        neighbour = self.model.grid.get_cell_list_contents(best_move)
+        while len(neighbour) != 0:
+            best_move = self.random.choice(possible_steps)
+            neighbour = self.model.grid.get_cell_list_contents(best_move)
+
+        # If close to apple to towards it
+        for p in possible_steps:
+            neighbour = self.model.grid.get_cell_list_contents(p)
+            if len(neighbour) == 0:
+                neighbourhood = self.model.grid.get_neighborhood(p, moore=True, include_center=True)
+                for n in self.model.grid.get_cell_list_contents(neighbourhood):
+                    if isinstance(n, Food) and n.supply > 0:
+                        best_move = p
+        return best_move
+
+
